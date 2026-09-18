f1d2f90e79f7dac9d694b765499f8a18783403de
kuharkristian20-arch <kuharkristian20@gmail.com>
2026-09-18 23:01:25 +0200

    Add new hero image

--- components/hero.tsx
+++ components/hero.tsx
@@ -7,12 +7,12 @@ export function Hero() {
   return (
     <section id="top" className="relative min-h-[92svh] w-full overflow-hidden">
       <Image
-        src="/images/hero.png"
+        src="/images/hero2.jpg"
         alt="Sunlit Adriatic seafront in Biograd na Moru with a stone waterfront and turquoise sea"
         fill
         priority
         sizes="100vw"
-        className="object-cover"
+        className="object-cover".
       />
       <div
         className="absolute inset-0 bg-gradient-to-b from-foreground/50 via-foreground/25 to-foreground/60"
